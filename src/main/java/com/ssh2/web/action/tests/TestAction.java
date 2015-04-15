2b1435bfc880a62d3ea5b6b39da792f936ee88c0
konglingyuan <konglingy_lady@sina.com>
2015-04-15 15:00:19 +0800

    Add Spring Suppot and Test pass

--- src/main/java/com/ssh2/web/action/tests/TestAction.java
+++ src/main/java/com/ssh2/web/action/tests/TestAction.java
@@ -2,10 +2,12 @@ package com.ssh2.web.action.tests;
 
 import org.apache.struts2.convention.annotation.Action;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 import com.opensymphony.xwork2.ActionSupport;
 import com.ssh2.service.HelloService;
 
+@Controller
 public class TestAction extends ActionSupport{
 	@Autowired
 	private HelloService helloService;
