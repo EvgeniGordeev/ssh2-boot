fa4da757b45d15843b8a4b17123c40d8532a0aba
konglingyuan <konglingy_lady@sina.com>
2015-04-15 15:26:03 +0800

    Add UserDAOImpl extend BaseDAO, implement UserDao

---
+++ src/main/java/com/ssh2/dao/impl/UserDAOImpl.java
@@ -0,0 +1,18 @@
+package com.ssh2.dao.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.ssh2.dao.BaseDAO;
+import com.ssh2.dao.UserDAO;
+import com.ssh2.po.User;
+
+@Repository
+public class UserDAOImpl extends BaseDAO implements UserDAO {
+
+	public List<User> findAll() {
+		String hql="from User";
+		return this.getHibernateTemplate().find(hql);
+	}
+}
