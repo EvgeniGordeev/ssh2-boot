be3680378ca1616dcef1a7bb74b32041d416cdfb
konglingyuan <konglingy_lady@sina.com>
2015-04-15 14:32:09 +0800

    Add HelloService interface impl

---
+++ src/main/java/com/ssh2/service/impl/HelloServiceImpl.java
@@ -0,0 +1,11 @@
+package com.ssh2.service.impl;
+
+import com.ssh2.service.HelloService;
+
+public class HelloServiceImpl implements HelloService{
+
+	public String sayHello() {
+		return "Hello World";
+	}
+
+}
