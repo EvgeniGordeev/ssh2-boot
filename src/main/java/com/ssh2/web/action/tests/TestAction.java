bf1bf02368ce59f9bab6c2e6fcb3a7d07297c3f6
konglingyuan <konglingy_lady@sina.com>
2015-04-15 13:41:07 +0800

    Add TestAction.java.struts convention

---
+++ src/main/java/com/ssh2/web/action/tests/TestAction.java
@@ -0,0 +1,13 @@
+package com.ssh2.web.action.tests;
+
+import org.apache.struts2.convention.annotation.Action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestAction extends ActionSupport{
+
+	@Action
+	public String showTest() {
+		return SUCCESS;
+	}
+}
